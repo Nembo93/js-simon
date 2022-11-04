d0cb196649648beb9155f3c1b37e55d8a5d22988
nembo93 <francesco.c193@libero.it>
2022-11-04 14:30:03 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+`use strict`
+
+const randomNum=[];
+let i = 0;
+while (i < 5){
+    let num = Math.floor(Math.random() * (100 - 1) + 1);
+    if (randomNum.includes(num) == false){
+        randomNum.push(num);
+    }
+    i++;
+}
+console.log(randomNum);
+document.getElementById(`random_number`).innerHTML = (randomNum);
